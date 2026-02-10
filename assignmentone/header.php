b1d1344905a0d3843dc4a867a505712aeccd6921
Mirka71 <200627729@student.georgianc.on.ca>
2026-02-09 19:39:26 -0500

    added character encoding, responsive layout, and linked stylesheet

---
+++ assignmentone/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <!-- Character encoding -->
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <!-- Responsive layout -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <?php wp_head(); ?>
+
+    <!-- Link the stylsheet -->
+    <link rel="stylesheet" href="<?php echo esc_url(home_url('wp-content/themes/Assignment1-2026/assignmentone/style.css')); ?>
+
+
